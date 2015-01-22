2de78ba8d096948a70cec1975507b1bacee979dc
hwclass <harderworking.class@gmail.com>
2015-01-22 22:57:07 +0200

    Added the searchInput module.

---
+++ app/public/searchInput.js
@@ -0,0 +1,102 @@
+/*
+ * Module :: amountOfDollar.js
+ * Info : Module for the input element of dollar currency.
+ */
+
+(function(Core) {
+	
+  Core.register('searchInput', function (sandbox) {
+
+    return {
+
+      /**
+       * init is a method that is used to make the listeners add and start
+       */
+      init : function () {
+        this.$amountOfDollar = sandbox.x('$')('#search_input');
+        this.addListeners();
+        this.listen();
+      },
+
+      /**
+       * addListeners is a method that is used to bind events
+       */
+      addListeners : function () {
+        this.$amountOfDollar.on('keypress', this.onKeypress.bind(this));
+      },
+
+      /**
+       * listen is a method that is used to listen custom events throughtout the application for this module
+       */
+      listen :  function () {
+        sandbox.listen('newEuroInputValue', this.newEuroInputValue, this);
+        sandbox.listen('newYenInputValue', this.newYenInputValue, this);
+      },
+
+      /**
+       * onKeypress is a method that is used to initialize functionalities after current event fires
+       */
+      onKeypress : function () {
+        var self = this;
+        setTimeout(function () {
+          var newValue = self.$amountOfDollar[0].value;
+          self.notify(newValue);
+        }, 0);
+      },
+
+      /**
+       * newEuroInputValue is a method to listen to the entering keys in the Euro currency input.
+       */
+      newEuroInputValue : function (data) {
+        var self = this;
+        var notifiedData = data.value;
+        sandbox.x('$').ajax({
+          type: 'GET',
+          url : sandbox.x('config').API.URL.CONVERT + sandbox.x('config').CURRENCY.EURO + '_' + sandbox.x('config').CURRENCY.DOLLAR,
+          crossDomain: true,
+          dataType: 'jsonp',
+          success: function(data) {
+            if (!isNaN(notifiedData)) {
+              self.$amountOfDollar[0].value = sandbox.x('numeral')(parseInt(notifiedData) * data.results['EUR_USD'].val).format('0,0.00[0]');
+            }
+          }
+        });
+      },
+
+      /**
+       * newYenInputValue is a method to listen to the entering keys in the Yen currency input.
+       */
+      newYenInputValue : function (data) {
+        var self = this;
+        var notifiedData = data.value;
+        sandbox.x('$').ajax({
+          type: 'GET',
+          url : sandbox.x('config').API.URL.CONVERT + sandbox.x('config').CURRENCY.YEN + '_' + sandbox.x('config').CURRENCY.DOLLAR,
+          crossDomain: true,
+          dataType: 'jsonp',
+          success: function(data) {
+            if (!isNaN(notifiedData)) {
+              self.$amountOfDollar[0].value = sandbox.x('numeral')(parseInt(notifiedData) * data.results['JPY_USD'].val).format("0,0.00[0]");
+            }
+          }
+        });
+      },
+
+      /**
+       * notify is a method that is used to inform the whole application that the current event fires
+       */
+      notify : function (newValue) {
+        sandbox.notify({
+          type : 'newDollarInputValue',
+          data : {
+            value : newValue
+          }
+        });
+      }
+
+    }
+
+  });
+
+}) (Core);
+
