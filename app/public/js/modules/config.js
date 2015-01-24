93c46c576228444bcc0308b9ade5ee8d19a506b8
hwclass <harderworking.class@gmail.com>
2015-01-24 19:32:26 +0200

    Modified the index view and config module, added searchInput module.

--- app/public/js/modules/config.js
+++ app/public/js/modules/config.js
@@ -4,5 +4,12 @@
  */
 
   var config = (function(){ 
-    return {}
+    return {
+      url: "https://editor.signavio.com/p/model/bdc48d12a3984cc5bef3ebb24f97dcb7",
+      authToken: "90e79e23cd8d3d7088e203d6a6e9d2f4f1d2c8109caceeb3b0de13f1e2e2_e2c760f97bed6bb72ef04d5c847347a5f430e668655ebc3492b5ca40727f84f0_24d2c752f424d6dc38e761a868e85e81994acedf84378d8629726ebafdd1",
+      overflowX: "fit",
+      overflowY: "fit",
+      zoomSlider: true,
+      focus : ''
+    }
   })();
--- app/public/js/modules/searchInput.js
+++ app/public/js/modules/searchInput.js
@@ -13,7 +13,7 @@
        * init is a method that is used to make the listeners add and start
        */
       init : function () {
-        this.$amountOfDollar = sandbox.x('$')('#search_input');
+        this.$searchInput = sandbox.x('$')('#search_input');
         this.addListeners();
         this.listen();
       },
@@ -22,16 +22,13 @@
        * addListeners is a method that is used to bind events
        */
       addListeners : function () {
-        this.$amountOfDollar.on('keypress', this.onKeypress.bind(this));
+        this.$searchInput.on('keypress', this.onKeypress.bind(this));
       },
 
       /**
        * listen is a method that is used to listen custom events throughtout the application for this module
        */
-      listen :  function () {
-        sandbox.listen('newEuroInputValue', this.newEuroInputValue, this);
-        sandbox.listen('newYenInputValue', this.newYenInputValue, this);
-      },
+      listen :  function () {},
 
       /**
        * onKeypress is a method that is used to initialize functionalities after current event fires
@@ -39,7 +36,7 @@
       onKeypress : function () {
         var self = this;
         setTimeout(function () {
-          var newValue = self.$amountOfDollar[0].value;
+          var newValue = self.$searchInput[0].value;
           self.notify(newValue);
         }, 0);
       },
@@ -47,7 +44,7 @@
       /**
        * newEuroInputValue is a method to listen to the entering keys in the Euro currency input.
        */
-      newEuroInputValue : function (data) {
+      newKeywordValue : function (data) {
         var self = this;
         var notifiedData = data.value;
         sandbox.x('$').ajax({
@@ -64,30 +61,11 @@
       },
 
       /**
-       * newYenInputValue is a method to listen to the entering keys in the Yen currency input.
-       */
-      newYenInputValue : function (data) {
-        var self = this;
-        var notifiedData = data.value;
-        sandbox.x('$').ajax({
-          type: 'GET',
-          url : sandbox.x('config').API.URL.CONVERT + sandbox.x('config').CURRENCY.YEN + '_' + sandbox.x('config').CURRENCY.DOLLAR,
-          crossDomain: true,
-          dataType: 'jsonp',
-          success: function(data) {
-            if (!isNaN(notifiedData)) {
-              self.$amountOfDollar[0].value = sandbox.x('numeral')(parseInt(notifiedData) * data.results['JPY_USD'].val).format("0,0.00[0]");
-            }
-          }
-        });
-      },
-
-      /**
        * notify is a method that is used to inform the whole application that the current event fires
        */
       notify : function (newValue) {
         sandbox.notify({
-          type : 'newDollarInputValue',
+          type : 'newKeywordValue',
           data : {
             value : newValue
           }
